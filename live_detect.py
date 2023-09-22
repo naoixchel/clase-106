3cf58d6b14ce961ae5b981e322e389cee78d2354
naoixchel <105891560+naoixchel@users.noreply.github.com>
2023-09-22 16:47:52 -0600

    Add files via upload

---
+++ live_detect.py
@@ -0,0 +1,29 @@
+# Importar la biblioteca OpenCV
+import cv2
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+# Definir un objeto de captura de video
+vid = cv2.VideoCapture(0)
+
+while(True):
+   
+    # Capturar el video cuadro por cuadro
+    ret, frame = vid.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray,1.1,5)
+   
+    for(x,y,w,h) in faces:
+        cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
+   
+    # Mostrar el cuadro resultante 
+    cv2.imshow("fotograma", frame) # OpenCV no acepta caracteres especiales como el acento en "Cámara"
+      
+    # Salir de la ventana a través de la barra espaciadora
+    if cv2.waitKey(25) == 32:
+        break
+  
+# Después del bucle, liberar el objeto capturado
+vid.release()
+
+# Destruir todas las ventanas
+cv2.destroyAllWindows()
